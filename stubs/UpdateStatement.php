e8cc56f88acdcbe9a21a7b274e26aa66dbc03fe8
Terrence Howard <chemisus@gmail.com>
2015-06-03 00:47:40 -0400

    testing orders

--- stubs/UpdateStatement.php
+++ stubs/UpdateStatement.php
@@ -11,6 +11,7 @@ class UpdateStatement implements Statement
                 ->fields([])
                 ->wheres([])
                 ->values([])
+                ->orders([])
                 ->limit(5);
         });
     }
